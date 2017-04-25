81c0b41bee7450f80f38351bb016ebfe1ec886a4
ckalpit <ckalpit34@gmail.com>
2017-04-25 17:04:53 +0530

    removed unnecessary files and converted project so that it can be opened in expo directly

--- index.android.js
+++ index.android.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { AppRegistry } from 'react-native'
 import AppRootContainer from './src/AppRootContainer'
+import Expo from 'expo'
 
 const HackathonStarter = () => (
   <AppRootContainer />
 )
 
-AppRegistry.registerComponent('HackathonStarter', () => HackathonStarter)
+// AppRegistry.registerComponent('HackathonStarter', () => HackathonStarter)
+Expo.registerRootComponent(HackathonStarter)
